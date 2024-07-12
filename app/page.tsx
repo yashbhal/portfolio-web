9a7b58040cd536ddfca93edf7989db6625b30322
Yash Bhalchandra <yashb1004@gmail.com>
2024-07-12 19:20:21 +0530

    hero page built

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
-// app/page.tsx
+import Image from "next/image";
+import heroImage from "../public/hero_image.jpeg";
+
 const Home = () => {
-  return <div>Hello!</div>;
+  return (
+    <div className="hero bg-primary min-h-screen text-primary-content flex items-center justify-center">
+      <div className="flex items-center justify-center w-full h-full p-4">
+        <Image
+          src={heroImage}
+          alt="Hero Image"
+          className="rounded-lg shadow-2xl object-contain"
+          layout="intrinsic"
+          width={650}
+          height={400}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
