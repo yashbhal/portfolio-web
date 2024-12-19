1c5970fc92b56b8abd9299ed7f54abf1a80ac0b0
Yash Bhalchandra <yashb1004@gmail.com>
2024-12-19 13:31:00 -0500

    centered footer for responsiveness issue

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,45 +2,48 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="footer bg-primary text-primary-content items-center p-4">
-      <aside className="grid-flow-col items-center">
-        <Image
-          src="/copyright-icon.svg"
-          alt="Email"
-          width={50}
-          height={50}
-          className="fill-current"
-        />
-        <p>© {new Date().getFullYear()} - Yash Bhalchandra</p>
-      </aside>
-      <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-        <a
-          href="https://www.linkedin.com/in/yashbhal"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+    <footer className="footer bg-primary text-primary-content p-4">
+      <div className="container mx-auto flex flex-col sm:flex-row items-center justify-between">
+        <div className="flex items-center gap-2 mb-4 sm:mb-0">
           <Image
-            src="/linkedin-icon.svg"
-            alt="LinkedIn"
-            width={35}
-            height={35}
+            src="/copyright-icon.svg"
+            alt="Copyright"
+            width={24}
+            height={24}
             className="fill-current"
           />
-        </a>
-        <a
-          href="https://github.com/yashbhal"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            src="/github-icon.svg"
-            alt="GitHub"
-            width={35}
-            height={35}
-            className="fill-current"
-          />
-        </a>
-      </nav>
+          <p>© {new Date().getFullYear()} - Yash Bhalchandra</p>
+        </div>
+
+        <div className="flex gap-4">
+          <a
+            href="https://www.linkedin.com/in/yashbhal"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/linkedin-icon.svg"
+              alt="LinkedIn"
+              width={24}
+              height={24}
+              className="fill-current"
+            />
+          </a>
+          <a
+            href="https://github.com/yashbhal"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/github-icon.svg"
+              alt="GitHub"
+              width={24}
+              height={24}
+              className="fill-current"
+            />
+          </a>
+        </div>
+      </div>
     </footer>
   );
 };
