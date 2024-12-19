0562ad936f73a703e0835e6f758e608c2fe18020
Yash Bhalchandra <yashb1004@gmail.com>
2024-12-19 16:35:00 -0500

    Updated projects

--- app/Work/workData.ts
+++ app/Work/workData.ts
@@ -1,58 +1,89 @@
 const workData = {
-    projects: [
-      {
-        id: 1,
-        title: "Portfolio Website",
-        description: "A portfolio website to showcase personal projects, work experience, and a blog. Built with Next.js, Typescript, Tailwind CSS, and DaisyUI",
-        imageUrl: "/work/software-development.jpg",
-        link: "https://github.com/yashbhal/portfolio-web",
-      },
-      {
-        id: 2,
-        title: "Grocery List Generator",
-        description: "An application that sends you ingredients to shop for based on recipes you select for the week. Built with Python, and PyQt6",
-        imageUrl: "/work/grocery-list.jpg",
-        link: "https://github.com/yashbhal/grocery-list-generator",
-      },
-      {
-        id: 3,
-        title: "Movie Suggestor",
-        description: "An older project that randomly suggests a movie to watch from the IMDB Top 250 Movies List. Built using Python and the BeautifulSoup library.",
-        imageUrl: "/work/movie.jpg",
-        link: "https://github.com/yashbhal/movie-finder",
-      },
-    ],
-    experiences: [
-      {
-        id: 1,
-        company: "Microchip Technology",
-        description: "Worked as part of the 16-bit Applications team, primarly developing new features, maintianing and improving the code for the 16-bit Bootloader libray, and the CryptoAuthentication Library.",
-        imageUrl: "/work/microchip.jpg",
-        link: "https://www.microchip.com/",
-      },
-      {
-        id: 2,
-        title: "KPIT",
-        description: "Worked in an Agile team to design and develop test scripts to automate the functional and stability testing of automotive infotainment systems.",
-        imageUrl: "/work/software-testing.jpg",
-        link: "https://www.kpit.com",
-      },
-      {
-        id: 3,
-        title: "Immersive Creation Studio",
-        description: "Building prototypical immersive XR platforms for learning environments.",
-        imageUrl: "/work/augmented-reality.jpg",
-        link: "https://xr.asu.edu",
-      },
-      {
-        id: 4,
-        title: "Meteor Studio",
-        description: "Worked on the design and development of an augmented reality software using Unity Game Engine for iOS, Android, and Microsoft Hololens.",
-        imageUrl: "/work/virtual-reality.jpg",
-        link: "https://meteor.ame.asu.edu/",
-      },
-    ],
-  };
-  
-  export default workData;
-  
+  projects: [
+    {
+      id: 1,
+      title:
+        "Advanced Indoor Scene Segmentation Using Open Vocabulary Classification",
+      description:
+        "Led development of an advanced 3D scene understanding system that achieved 67.8% accuracy in open-vocabulary object classification across 250 indoor object categories, by integrating Semantic SAM for segmentation and SigLIP for classification.",
+      imageUrl: "/work/software-development.jpg",
+      link: "https://loggs.github.io/cs7641-project-proposal/",
+    },
+    {
+      id: 2,
+      title: "Passive Haptic Stroke Rehabilitation Games",
+      description:
+        "(No link) Developed and conducted user testing for two Unity and Mediapipe based rehabilitation games, designed to use gestures to measure upper limb mobility improvements in stroke and sclerosis patients using a novel vibrotactile glove.",
+      imageUrl: "/work/software-development.jpg",
+      link: "https://github.com/yashbhal/",
+    },
+    {
+      id: 3,
+      title: "Privacy Focused AI Journal Application",
+      description:
+        "(In Dev) Creating a secure digital journaling system with Next.js and PostgreSQL database integration. Leverages AI capabilities using Langchain and Llama to transform user entries into entertaining monthly adventures.",
+      imageUrl: "/work/software-development.jpg",
+      link: "https://github.com/yashbhal/",
+    },
+    {
+      id: 4,
+      title: "Portfolio Website",
+      description:
+        "A portfolio website to showcase personal projects, work experience, and a blog. Built with Next.js, Typescript, Tailwind CSS, and DaisyUI",
+      imageUrl: "/work/software-development.jpg",
+      link: "https://github.com/yashbhal/portfolio-web",
+    },
+    {
+      id: 2,
+      title: "Grocery List Generator",
+      description:
+        "An application that sends you ingredients to shop for based on recipes you select for the week. Built with Python, and PyQt6",
+      imageUrl: "/work/grocery-list.jpg",
+      link: "https://github.com/yashbhal/grocery-list-generator",
+    },
+    {
+      id: 3,
+      title: "Movie Suggestor",
+      description:
+        "An older project that randomly suggests a movie to watch from the IMDB Top 250 Movies List. Built using Python and the BeautifulSoup library.",
+      imageUrl: "/work/movie.jpg",
+      link: "https://github.com/yashbhal/movie-finder",
+    },
+  ],
+  experiences: [
+    {
+      id: 1,
+      company: "Microchip Technology",
+      description:
+        "Worked as part of the 16-bit Applications team, primarly developing new features, maintianing and improving the code for the 16-bit Bootloader libray, and the CryptoAuthentication Library.",
+      imageUrl: "/work/microchip.jpg",
+      link: "https://www.microchip.com/",
+    },
+    {
+      id: 2,
+      title: "KPIT",
+      description:
+        "Worked in an Agile team to design and develop test scripts to automate the functional and stability testing of automotive infotainment systems.",
+      imageUrl: "/work/software-testing.jpg",
+      link: "https://www.kpit.com",
+    },
+    {
+      id: 3,
+      title: "Immersive Creation Studio",
+      description:
+        "Building prototypical immersive XR platforms for learning environments.",
+      imageUrl: "/work/augmented-reality.jpg",
+      link: "https://xr.asu.edu",
+    },
+    {
+      id: 4,
+      title: "Meteor Studio",
+      description:
+        "Worked on the design and development of an augmented reality software using Unity Game Engine for iOS, Android, and Microsoft Hololens.",
+      imageUrl: "/work/virtual-reality.jpg",
+      link: "https://meteor.ame.asu.edu/",
+    },
+  ],
+};
+
+export default workData;
