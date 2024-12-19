18510667c6e86b05aac57f574f97a42590f9db0c
Yash Bhalchandra <yashb1004@gmail.com>
2024-12-19 17:30:53 -0500

    Updated About

--- app/About/AboutCard.tsx
+++ app/About/AboutCard.tsx
@@ -13,17 +13,17 @@ const AboutCard = () => {
       <div className="card-body text-center">
         <h1 className="text-3xl font-bold mb-4">More about Yash</h1>
         <p className="mb-4">
-          I graduated from Arizona State University with an undergraduate degree
-          in Computer Science and have experience working a software tester in
-          the automotive industry at KPIT, and then working as a application
-          developer for almost 2 years in the world of embedded products at
-          Microchip Technology.
+          I have around 2 years of experience working full-time as a software
+          developer, and an undergraduate degree in Computer Science from
+          Arizona State University. I love working in interdisciplinary teams
+          towards building creative and intuitive products that solve problems.
         </p>
         <p className="mb-4">
           Currently, I am a graduate student studying Computer Science at
-          Georgia Institute of Technology. I love working in interdisciplinary
-          teams towards building creative and intuitive products that solve
-          problems.
+          Georgia Institute of Technology. I am open to work and actively
+          looking for internships and co-ops starting Summer 2025. Feel free to
+          email me at ashbhal@gatech.edu or reach out to me on LinkedIn with
+          opportunities or even for just a chat!
         </p>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="p-4">
@@ -31,12 +31,8 @@ const AboutCard = () => {
             <p>Soccer | Art | Reading</p>
           </div>
           <div className="p-4">
-            <h2 className="text-xl font-semibold">Currently Learning</h2>
-            <p>Artificial Intelligence | Python | Next.js</p>
-          </div>
-          <div className="p-4">
             <h2 className="text-xl font-semibold">Current Projects</h2>
-            <p>Portfolio Website | Grocery List Generator</p>
+            <p>QuestLife - A Privacy Focused Journalling App</p>
           </div>
         </div>
       </div>
